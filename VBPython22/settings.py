f85f5266435b35570db39f85f87a9f457c39dcd3
ibrahim ediz <ibrahim.ediz@gazi.edu.tr>
2020-10-25 12:07:11 +0200

    25102020_2

--- VBPython22/settings.py
+++ VBPython22/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS =  [
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath("epostalar"))
